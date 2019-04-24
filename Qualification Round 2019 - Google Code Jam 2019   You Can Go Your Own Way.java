8750c77fbff651bfb8cc2a3a9953c522521c2860
Rayan Aradha <46860744+rayanaradha@users.noreply.github.com>
2019-04-25 02:06:23 +0530

    Create Qualification Round 2019 - Google Code Jam 2019   You Can Go Your Own Way.java

---
+++ Qualification Round 2019 - Google Code Jam 2019   You Can Go Your Own Way.java
@@ -0,0 +1,23 @@
+import java.util.*;
+    import java.io.*;
+    public class Solution {
+
+       public static String ans(String x){
+          String x1=x.replace('E','T');
+          String x2=x1.replace('S','E');
+          String x3=x2.replace('T','S');
+           return x3;
+
+       } 
+
+      public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int t = in.nextInt();
+        for (int i = 1; i <= t; ++i) {
+          int n = in.nextInt();
+          String r = in.next();
+          System.out.println("Case #" + i + ": " + ans(r));
+         
+        }
+      }
+    }
